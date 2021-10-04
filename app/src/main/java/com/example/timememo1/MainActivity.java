9d968c47a4eaa50b8b00de0e128d70539b54f0ae
sunnyblack1202 <sunnypolimaru1221@gmail.com>
2021-10-05 03:25:08 +0900

    リストの行ごとのレイアウト作成前まで

--- app/src/main/java/com/example/timememo1/MainActivity.java
+++ app/src/main/java/com/example/timememo1/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.timememo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvMemo = findViewById(R.id.lvMemo);
+
+        List<Map<String, String>> memoList = new ArrayList<>();
+        Map<String, String> memo = new HashMap<>();
+        memo.put("title", "8時間ダイエット");
+        memo.put("settime", "8時間");
+        memo.put("starttime", "08:30");
+        memo.put("endingtime", "16:30");
+        memoList.add(memo);
+
+        memo = new HashMap<>();
+        memo.put("title", "MVスミン");
+        memo.put("settime", "30分");
+        memo.put("starttime", "13:40");
+        memo.put("endingtime", "14:10");
+        memoList.add(memo);
+
+        memo = new HashMap<>();
+        memo.put("title", "食後");
+        memo.put("settime", "30分");
+        memo.put("starttime", "16:20");
+        memo.put("endingtime", "16:50");
+        memoList.add(memo);
     }
 }
