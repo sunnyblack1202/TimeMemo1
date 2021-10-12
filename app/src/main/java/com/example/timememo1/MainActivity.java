15fd1ec57838970f9d7dff9abd2c4164a8e7c487
sunnyblack1202 <sunnypolimaru1221@gmail.com>
2021-10-12 20:57:23 +0900

    ちょっと変更 EdiitActivity作成

--- app/src/main/java/com/example/timememo1/MainActivity.java
+++ app/src/main/java/com/example/timememo1/MainActivity.java
@@ -13,6 +13,9 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] from = {"title", "settime", "starttime", "endingtime"};
+    private static final int[] to = {R.id.tvTitle, R.id.tvSettime, R.id.tvStarttime, R.id.tvEndingtime};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvMemo = findViewById(R.id.lvMemo);
 
+        List<Map<String, String>> _memoList = createMemoList();
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, _memoList,
+                R.layout.memo_list_item, from, to);
+
+        lvMemo.setAdapter(adapter);
+    }
+
+    private List<Map<String, String>> createMemoList() {
         List<Map<String, String>> memoList = new ArrayList<>();
         Map<String, String> memo = new HashMap<>();
         memo.put("title", "8時間ダイエット");
@@ -42,12 +54,6 @@ public class MainActivity extends AppCompatActivity {
         memo.put("endingtime", "16:50");
         memoList.add(memo);
 
-        String[] from = {"title", "settime", "starttime", "endingtime"};
-        int[] to = {R.id.tvTitle, R.id.tvSettime, R.id.tvStarttime, R.id.tvEndingtime};
-
-        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, memoList,
-                R.layout.memo_list_item, from, to);
-
-        lvMemo.setAdapter(adapter);
+        return memoList;
     }
 }
